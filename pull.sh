1fadefb50b125b36cc1b7d80c78d120f776818bf
Martyn Pratt <martyni.pratt@digital.homeoffice.gov.uk>
2019-09-17 14:22:23 +0100

    added echo

--- pull.sh
+++ pull.sh
@@ -3,3 +3,8 @@ curl -H \
   -X POST -d\
   "{ \"title\":\"${TRAVIS_BRNACH}\",\"head\":\"${TRAVIS_BRANCH}\",\"base\":\"master\", \"body\": \"${TRAVIS_COMMIT_MESSAGE}\", }"\
   "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls"
+echo curl -H \
+  "Authorization: token  afa7e54e1999c6f0a51ced7a6831113b3987a34a"\
+  -X POST -d\
+  "{ \"title\":\"${TRAVIS_BRNACH}\",\"head\":\"${TRAVIS_BRANCH}\",\"base\":\"master\", \"body\": \"${TRAVIS_COMMIT_MESSAGE}\", }"\
+  "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls"
