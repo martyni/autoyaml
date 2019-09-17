f548ca90b169f2f6563cefd05fac61df45f00daf
Martyn Pratt <martyni.pratt@digital.homeoffice.gov.uk>
2019-09-17 14:49:57 +0100

    oneline curl

--- pull.sh
+++ pull.sh
@@ -1,10 +1,3 @@
-curl -H \
-  "Authorization: token $git"\ 
-  -X POST -d\
-  "{ \"title\":\"${TRAVIS_BRNACH}\",\"head\":\"${TRAVIS_BRANCH}\",\"base\":\"master\", \"body\": \"${TRAVIS_COMMIT_MESSAGE}\" }"\
-  "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls"
-echo curl -H \
-  "Authorization: token  afa7e54e1999c6f0a51ced7a6831113b3987a34a"\
-  -X POST -d\
-  "{ \"title\":\"${TRAVIS_BRANCH}\",\"head\":\"${TRAVIS_BRANCH}\",\"base\":\"master\", \"body\": \"${TRAVIS_COMMIT_MESSAGE}\" }"\
-  "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls"
+curl -H "Authorization: token $git" -X POST -d "{ \"title\":\"${TRAVIS_BRNACH}\",\"head\":\"${TRAVIS_BRANCH}\",\"base\":\"master\", \"body\": \"${TRAVIS_COMMIT_MESSAGE}\" }" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls"
+
+echo curl -H "Authorization: token  $git" -X POST -d "{ \"title\":\"${TRAVIS_BRANCH}\",\"head\":\"${TRAVIS_BRANCH}\",\"base\":\"master\", \"body\": \"${TRAVIS_COMMIT_MESSAGE}\" }" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls"
