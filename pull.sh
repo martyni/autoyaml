2c0ea64b0aec43acca9a2c1426f7d093bb0974ba
Martyn Pratt <martyni.pratt@digital.homeoffice.gov.uk>
2019-09-17 16:32:46 +0100

    removed echo

--- pull.sh
+++ pull.sh
@@ -1,3 +1,2 @@
 curl -H "Authorization: token $githubtoken" -X POST -d "{ \"title\":\"${TRAVIS_BRANCH}\",\"head\":\"${TRAVIS_BRANCH}\",\"base\":\"master\", \"body\": \"${TRAVIS_COMMIT_MESSAGE}\" }" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls"
 
-echo curl -H "Authorization: token  $githubtoken" -X POST -d "{ \"title\":\"${TRAVIS_BRANCH}\",\"head\":\"${TRAVIS_BRANCH}\",\"base\":\"master\", \"body\": \"${TRAVIS_COMMIT_MESSAGE}\" }" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls"
