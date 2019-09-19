994ab2c2e5f1b1b015fd24c9e0dffc552f754dc4
Martyn Pratt <martyni.pratt@digital.homeoffice.gov.uk>
2019-09-19 11:08:12 +0100

    removed unnecesary steps and added text to pull.sh

--- pull.sh
+++ pull.sh
@@ -1,5 +1,6 @@
 if [ "${TRAVIS_BRANCH}" == "master" ]
-   then exit
+   then echo On master exiting now
+   exit
 fi
  
 echo curl -H "Authorization: token ${token}" -X POST -d "{ \"title\":\"${TRAVIS_BRANCH}\",\"head\":\"${TRAVIS_BRANCH}\",\"base\":\"master\", \"body\": \"${TRAVIS_COMMIT_MESSAGE}\" }" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls"
